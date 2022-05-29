963512e087e804cf57dafe07a0485c9285de9887
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-29 18:31:58 +0200

    Added SafeOwner class

---
+++ Safe/SafeOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe
+{
+    public class SafeOwner
+    {
+        private string valuables = "";
+        public void ReceiveContents(string safeContents)
+        {
+            valuables = safeContents;
+            Console.WriteLine($"Moje {valuables}! Dziękuję za zwrot!");
+        }
+    }
+}
