1b84c7351f4f37f81d613c204fb068a8b1f83f58
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-29 18:45:33 +0200

    Added Main method

--- Safe/Program.cs
+++ Safe/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey(true);
         }
     }
 }
