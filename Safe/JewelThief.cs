7c17fc46feef35df374a5a48d629bad396fd2614
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-29 18:40:47 +0200

    Added JewelThief class

---
+++ Safe/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe
+{
+    public class JewelThief : Locksmith
+    {
+        private string stolenJewels;
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"Kradnę kosztowności! Ukradłem: {stolenJewels}");
+        }
+    }
+}
